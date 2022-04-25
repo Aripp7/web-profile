f6def1f7cd472a089936ee54dba8fdbe6c836648
Ahmad Arif <ahmadarif9085@gmail.com>
2022-04-25 21:01:22 +0700

    new

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  
+  content: ['index.html'],
+  theme: {
+    container:{
+center: true,
+padding : '16px',
+    },
+    extend: {
+
+      colors: {
+        primary: '#1E90FF',
+        secondary: '#64748B',
+        dark: '#0F172A'
+      },
+      screen:{
+        '2xl' : '1320px'
+      }
+    },
+  },
+  plugins: [],
+}
